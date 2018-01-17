225f05e0c2fdef85b96a539f90c19024660ba50a
sysoper <michrzep@interia.pl>
2018-01-17 12:46:15 +0100

    znaczny postep w rozumieniu list, napisalem podstawowe funkcje

---
+++ C++/dynamic_mem_exc/cmake-build-debug/CMakeFiles/dynamic_mem_exc.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/dynamic_mem_exc.dir/main.cpp.obj"
+  "CMakeFiles/dynamic_mem_exc.dir/bool_array.cpp.obj"
+  "CMakeFiles/dynamic_mem_exc.dir/debug_new.cpp.obj"
+  "CMakeFiles/dynamic_mem_exc.dir/mem_pool_base.cpp.obj"
+  "CMakeFiles/dynamic_mem_exc.dir/static_mem_pool.cpp.obj"
+  "dynamic_mem_exc.pdb"
+  "dynamic_mem_exc.exe"
+  "dynamic_mem_exc.exe.manifest"
+  "libdynamic_mem_exc.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/dynamic_mem_exc.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
